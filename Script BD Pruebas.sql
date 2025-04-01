e3a027d782f53af1d6cf14eaa9a01ee973308322
Sergio González <sergiogonzalezalcantara@gmail.com>
2025-04-01 21:06:37 +0200

    New database script

--- Script BD Pruebas.sql
+++ Script BD Pruebas.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-03-2025 a las 23:43:32
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 01-04-2025 a las 21:04:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -62,7 +62,163 @@ INSERT INTO `champions` (`id`, `name`) VALUES
 (11, 'Ashe'),
 (12, 'Aurelion Sol'),
 (13, 'Aurora'),
-(14, 'Azir');
+(14, 'Azir'),
+(17, 'Bard'),
+(18, 'Bel\'Veth'),
+(19, 'Blitzcrank'),
+(20, 'Brand'),
+(176, 'Braum'),
+(22, 'Briar'),
+(23, 'Caitlyn'),
+(24, 'Camille'),
+(25, 'Cassiopeia'),
+(26, 'Cho\'Gath'),
+(27, 'Corki'),
+(28, 'Darius'),
+(29, 'Diana'),
+(30, 'Dr. Mundo'),
+(31, 'Draven'),
+(32, 'Ekko'),
+(33, 'Elise'),
+(34, 'Evelynn'),
+(35, 'Ezreal'),
+(36, 'Fiddlesticks'),
+(37, 'Fiora'),
+(38, 'Fizz'),
+(39, 'Galio'),
+(40, 'Gangplank'),
+(42, 'Garen'),
+(43, 'Gnar'),
+(44, 'Gragas'),
+(45, 'Graves'),
+(46, 'Gwen'),
+(47, 'Hecarim'),
+(48, 'Heimerdinger'),
+(49, 'Hwei'),
+(50, 'Illaoi'),
+(51, 'Irelia'),
+(52, 'Ivern'),
+(53, 'Janna'),
+(54, 'Jarvan IV'),
+(55, 'Jax'),
+(56, 'Jayce'),
+(57, 'Jhin'),
+(58, 'Jinx'),
+(59, 'K\'Sante'),
+(60, 'Kai\'Sa'),
+(61, 'Kalista'),
+(62, 'Karma'),
+(63, 'Karthus'),
+(64, 'Kassadin'),
+(65, 'Katarina'),
+(66, 'Kayle'),
+(67, 'Kayn'),
+(68, 'Kennen'),
+(69, 'Kha\'Zix'),
+(70, 'Kindred'),
+(71, 'Kled'),
+(72, 'Kog\'Maw'),
+(73, 'LeBlanc'),
+(74, 'Lee Sin'),
+(75, 'Leona'),
+(76, 'Lillia'),
+(77, 'Lissandra'),
+(78, 'Lucian'),
+(79, 'Lulu'),
+(80, 'Lux'),
+(81, 'Malphite'),
+(82, 'Malzahar'),
+(83, 'Maokai'),
+(84, 'Master Yi'),
+(85, 'Mel'),
+(86, 'Milio'),
+(87, 'Miss Fortune'),
+(88, 'Mordekaiser'),
+(89, 'Morgana'),
+(90, 'Naafiri'),
+(91, 'Nami'),
+(92, 'Nasus'),
+(93, 'Nautilus'),
+(94, 'Neeko'),
+(95, 'Nidalee'),
+(96, 'Nilah'),
+(97, 'Nocturne'),
+(98, 'Nunu & Willump'),
+(99, 'Olaf'),
+(100, 'Orianna'),
+(101, 'Ornn'),
+(102, 'Pantheon'),
+(103, 'Poppy'),
+(104, 'Pyke'),
+(105, 'Qiyana'),
+(106, 'Quinn'),
+(107, 'Rakan'),
+(108, 'Rammus'),
+(109, 'Rek\'Sai'),
+(110, 'Rell'),
+(111, 'Renata Glasc'),
+(112, 'Renekton'),
+(113, 'Rengar'),
+(114, 'Riven'),
+(115, 'Rumble'),
+(116, 'Ryze'),
+(117, 'Samira'),
+(118, 'Sejuani'),
+(119, 'Senna'),
+(120, 'Seraphine'),
+(121, 'Sett'),
+(122, 'Shaco'),
+(123, 'Shen'),
+(124, 'Shyvanna'),
+(125, 'Singed'),
+(126, 'Sion'),
+(127, 'Sivir'),
+(128, 'Skarner'),
+(129, 'Smolder'),
+(130, 'Sona'),
+(131, 'Soraka'),
+(132, 'Swain'),
+(133, 'Sylas'),
+(134, 'Syndra'),
+(135, 'Tahm Kench'),
+(136, 'Taliyah'),
+(137, 'Talon'),
+(138, 'Taric'),
+(139, 'Teemo'),
+(140, 'Thresh'),
+(141, 'Tristana'),
+(142, 'Trundle'),
+(143, 'Tryndamere'),
+(144, 'Twisted Fate'),
+(145, 'Twitch'),
+(146, 'Udyr'),
+(147, 'Urgot'),
+(148, 'Varus'),
+(149, 'Vayne'),
+(150, 'Veigar'),
+(151, 'Vel\'Koz'),
+(152, 'Vex'),
+(153, 'Vi'),
+(154, 'Viego'),
+(155, 'Viktor'),
+(156, 'Vladimir'),
+(157, 'Volibear'),
+(158, 'Warwick'),
+(160, 'Wukong'),
+(161, 'Xayah'),
+(162, 'Xerath'),
+(163, 'Xin Zhao'),
+(164, 'Yasuo'),
+(165, 'Yone'),
+(166, 'Yorick'),
+(167, 'Yuumi'),
+(168, 'Zac'),
+(169, 'Zed'),
+(170, 'Zeri'),
+(171, 'Ziggs'),
+(172, 'Zilean'),
+(173, 'Zoe'),
+(174, 'Zyra');
 
 -- --------------------------------------------------------
 
@@ -90,34 +246,35 @@ CREATE TABLE `players` (
   `tag` varchar(50) NOT NULL,
   `description` text DEFAULT NULL,
   `duration` date DEFAULT NULL,
-  `hoster` varchar(50) NOT NULL
+  `hoster` varchar(50) NOT NULL,
+  `permanent` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `players`
 --
 
-INSERT INTO `players` (`id`, `username`, `tag`, `description`, `duration`, `hoster`) VALUES
-(1, 'player1', '#EUW1', 'Description for player1', '2023-01-01', 'hoster_user1'),
-(2, 'player2', '#EUW2', 'Description for player2', '2023-01-02', 'hoster_user1'),
-(3, 'player3', '#EUW3', 'Description for player3', '2023-01-03', 'hoster_user1'),
-(4, 'player4', '#EUW4', 'Description for player4', '2023-01-04', 'hoster_user1'),
-(5, 'player5', '#EUW5', 'Description for player5', '2023-01-05', 'hoster_user1'),
-(6, 'player6', '#EUW6', 'Description for player6', '2023-01-06', 'hoster_user1'),
-(7, 'player7', '#EUW7', 'Description for player7', '2023-01-07', 'hoster_user1'),
-(8, 'player8', '#EUW8', 'Description for player8', '2023-01-08', 'hoster_user1'),
-(9, 'player9', '#EUW9', 'Description for player9', '2023-01-09', 'hoster_user1'),
-(10, 'player10', '#EUW10', 'Description for player10', '2023-01-10', 'hoster_user1'),
-(11, 'player11', '#EUW11', 'Description for player11', '2023-01-11', 'hoster_user1'),
-(12, 'player12', '#EUW12', 'Description for player12', '2023-01-12', 'hoster_user1'),
-(13, 'player13', '#EUW13', 'Description for player13', '2023-01-13', 'hoster_user1'),
-(14, 'player14', '#EUW14', 'Description for player14', '2023-01-14', 'hoster_user1'),
-(15, 'player15', '#EUW15', 'Description for player15', '2023-01-15', 'hoster_user1'),
-(16, 'player16', '#EUW16', 'Description for player16', '2023-01-16', 'hoster_user1'),
-(17, 'player17', '#EUW17', 'Description for player17', '2023-01-17', 'hoster_user1'),
-(18, 'player18', '#EUW18', 'Description for player18', '2023-01-18', 'hoster_user1'),
-(19, 'player19', '#EUW19', 'Description for player19', '2023-01-19', 'hoster_user1'),
-(20, 'player20', '#EUW20', 'Description for player20', '2023-01-20', 'hoster_user1');
+INSERT INTO `players` (`id`, `username`, `tag`, `description`, `duration`, `hoster`, `permanent`) VALUES
+(1, 'player1', '#EUW1', 'Description for player1', '2025-04-15', 'Jesusito', 1),
+(2, 'player2', '#EUW2', 'Description for player2', '2025-04-18', 'Jesusito', 1),
+(3, 'player3', '#EUW3', 'Description for player3', '2025-04-19', 'Jesusito', 0),
+(4, 'player4', '#EUW4', 'Description for player4', '2025-04-24', 'Jesusito', 0),
+(5, 'player5', '#EUW5', 'Description for player5', '2025-04-12', 'Jesusito', 0),
+(6, 'player6', '#EUW6', 'Description for player6', '2025-04-05', 'Jesusito', 1),
+(7, 'player7', '#EUW7', 'Description for player7', '2025-04-10', 'Jesusito', 1),
+(8, 'player8', '#EUW8', 'Description for player8', '2025-04-30', 'Jesusito', 0),
+(9, 'player9', '#EUW9', 'Description for player9', '2025-04-28', 'Jesusito', 1),
+(10, 'player10', '#EUW10', 'Description for player10', '2025-04-15', 'Jesusito', 0),
+(11, 'player11', '#EUW11', 'Description for player11', '2025-04-22', 'Jesusito', 0),
+(12, 'player12', '#EUW12', 'Description for player12', '2025-04-07', 'Jesusito', 1),
+(13, 'player13', '#EUW13', 'Description for player13', '2025-04-01', 'Jesusito', 0),
+(14, 'player14', '#EUW14', 'Description for player14', '2025-04-29', 'Jesusito', 1),
+(15, 'player15', '#EUW15', 'Description for player15', '2025-04-30', 'Jesusito', 1),
+(16, 'player16', '#EUW16', 'Description for player16', '2025-04-15', 'Jesusito', 1),
+(17, 'player17', '#EUW17', 'Description for player17', '2025-04-02', 'Jesusito', 0),
+(18, 'player18', '#EUW18', 'Description for player18', '2025-05-05', 'Jesusito', 0),
+(19, 'player19', '#EUW19', 'Description for player19', '2025-06-20', 'Jesusito', 1),
+(20, 'player20', '#EUW20', 'Description for player20', '2025-06-15', 'Jesusito', 0);
 
 -- --------------------------------------------------------
 
@@ -139,8 +296,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `type`) VALUES
 (1, 'Pyky', 'pykylolcgm@gmail.com', '1234', 'ADMIN'),
-(2, 'hoster_user1', 'hoster1@example.com', 'securepassword2', 'HOSTER'),
-(3, 'hoster_user2', 'hoster2@example.com', 'securepassword3', 'HOSTER');
+(2, 'Jesusito', 'hoster1@example.com', '4321', 'HOSTER'),
+(7, 'Keys', 'keyslolcgm@gmail.com', 'abam1234', 'ADMIN'),
+(8, 'Aeryn', 'aerynlolcgm@gmail.com', 'abam1234', 'HOSTER'),
+(9, 'CannonMinion', 'cannonlolcgm@gmail.com', 'abam1234', 'HOSTER'),
+(10, 'Griefer', 'grieferlolcgm@gmail.com', 'abam1234', 'HOSTER');
 
 --
 -- Índices para tablas volcadas
@@ -194,7 +354,7 @@ ALTER TABLE `bans`
 -- AUTO_INCREMENT de la tabla `champions`
 --
 ALTER TABLE `champions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=177;
 
 --
 -- AUTO_INCREMENT de la tabla `historial`
@@ -212,7 +372,7 @@ ALTER TABLE `players`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
